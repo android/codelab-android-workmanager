b30f7deca3bd1afd14205b30a4734813bbbf569f
Pietro Maggi <pmaggi@google.com>
2020-01-15 16:57:36 +0000

    Adds ViewBinding and fix a missing call to super in onActivityResult

--- app/src/main/java/com/example/background/SelectImageActivity.kt
+++ app/src/main/java/com/example/background/SelectImageActivity.kt
@@ -23,11 +23,13 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.LayoutInflater
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
+import com.example.background.databinding.ActivitySelectBinding
 import java.util.Arrays
 import timber.log.Timber
 
@@ -46,11 +48,12 @@ class SelectImageActivity : AppCompatActivity() {
     private var permissionRequestCount: Int = 0
     private lateinit var selectImageButton: Button
 
+    private lateinit var binding: ActivitySelectBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select)
-
-        selectImageButton = findViewById(R.id.selectImage)
+        binding = ActivitySelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         savedInstanceState?.let {
             permissionRequestCount = it.getInt(KEY_PERMISSIONS_REQUEST_COUNT, 0)
@@ -60,7 +63,7 @@ class SelectImageActivity : AppCompatActivity() {
         requestPermissionsIfNecessary()
 
         // Create request to get image from filesystem when button clicked
-        selectImageButton.setOnClickListener {
+        binding.selectImage.setOnClickListener {
             val chooseIntent = Intent(
                     Intent.ACTION_PICK,
                     MediaStore.Images.Media.EXTERNAL_CONTENT_URI
