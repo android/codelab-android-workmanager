eea551beb9cd9c1dc7284c76927014721eb6c71d
Pietro Maggi <pmaggi@google.com>
2020-04-03 15:57:50 +0100

    Add a WorkManager’s custom configuration

--- app/src/main/java/com/example/background/BlurApplication.kt
+++ app/src/main/java/com/example/background/BlurApplication.kt
@@ -17,10 +17,24 @@
 package com.example.background
 
 import android.app.Application
+import androidx.work.Configuration
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
-class BlurApplication() : Application() {
+class BlurApplication() : Application(), Configuration.Provider {
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return if (BuildConfig.DEBUG) {
+            Configuration.Builder()
+                    .setMinimumLoggingLevel(android.util.Log.DEBUG)
+                    .build()
+        } else {
+            Configuration.Builder()
+                    .setMinimumLoggingLevel(android.util.Log.ERROR)
+                    .build()
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
