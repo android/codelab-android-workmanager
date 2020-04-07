cfa2a78d9c33af15b16a49d1617e63a9ba6fcdc4
Pietro Maggi <pmaggi@google.com>
2020-04-07 16:55:51 +0100

    Fixes Log levels import.

--- app/src/main/java/com/example/background/BlurApplication.kt
+++ app/src/main/java/com/example/background/BlurApplication.kt
@@ -20,17 +20,19 @@ import android.app.Application
 import androidx.work.Configuration
 import timber.log.Timber
 import timber.log.Timber.DebugTree
+import android.util.Log.DEBUG
+import android.util.Log.ERROR
 
 class BlurApplication() : Application(), Configuration.Provider {
 
     override fun getWorkManagerConfiguration(): Configuration {
         return if (BuildConfig.DEBUG) {
             Configuration.Builder()
-                    .setMinimumLoggingLevel(android.util.Log.DEBUG)
+                    .setMinimumLoggingLevel(DEBUG)
                     .build()
         } else {
             Configuration.Builder()
-                    .setMinimumLoggingLevel(android.util.Log.ERROR)
+                    .setMinimumLoggingLevel(ERROR)
                     .build()
         }
     }
