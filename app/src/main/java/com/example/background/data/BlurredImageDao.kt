1b45db5b981ac367f8287d9074b89df914590fb7
Florina Muntenescu <florinam@google.com>
2020-11-17 12:24:49 +0000

    Using CoroutinesWorker to save the blurred image in the database

---
+++ app/src/main/java/com/example/background/data/BlurredImageDao.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.background.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * The Room Magic is in this file, where you map a method call to an SQL query.
+ *
+ * When you are using complex data types, such as Date, you have to also supply type converters.
+ * To keep this example basic, no types that require type converters are used.
+ * See the documentation at
+ * https://developer.android.com/topic/libraries/architecture/room.html#type-converters
+ */
+
+@Dao
+interface BlurredImageDao {
+
+    @Query("SELECT * FROM images_table")
+    fun getBlurredImages(): Flow<List<BlurredImage>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(blurredImage: BlurredImage)
+}
--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -19,45 +19,56 @@ package com.example.background.workers
 import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.text.TextUtils
+import androidx.work.CoroutineWorker
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
 import com.example.background.KEY_IMAGE_URI
+import com.example.background.data.BlurredImage
+import com.example.background.data.ImagesDatabase
 import timber.log.Timber
 
-class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
 
-    override fun doWork(): Result {
-        val appContext = applicationContext
-
+    override suspend fun doWork(): Result {
         val resourceUri = inputData.getString(KEY_IMAGE_URI)
 
-        makeStatusNotification("Blurring image", appContext)
+        makeStatusNotification("Blurring image", applicationContext)
         sleep()
 
         return try {
-            if (TextUtils.isEmpty(resourceUri)) {
+            if (resourceUri.isNullOrEmpty()) {
                 Timber.e("Invalid input uri")
                 throw IllegalArgumentException("Invalid input uri")
             }
 
-            val resolver = appContext.contentResolver
-
-            val picture = BitmapFactory.decodeStream(
-                    resolver.openInputStream(Uri.parse(resourceUri)))
-
-            val output = blurBitmap(picture, appContext)
-
-            // Write bitmap to a temp file
-            val outputUri = writeBitmapToFile(appContext, output)
-
-            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
-
+            val outputData = blurAndSaveImage(resourceUri)
+            recordImageSaved(resourceUri)
             Result.success(outputData)
         } catch (throwable: Throwable) {
             Timber.e(throwable, "Error applying blur")
             Result.failure()
         }
+    }
+
+    private suspend fun recordImageSaved(resourceUri: String) {
+        val imageDao = ImagesDatabase.getDatabase(applicationContext).blurredImageDao()
+        imageDao.insert(BlurredImage(resourceUri))
+    }
+
+    private fun blurAndSaveImage(resourceUri: String): Data {
+        val resolver = applicationContext.contentResolver
+
+        val picture = BitmapFactory.decodeStream(
+            resolver.openInputStream(Uri.parse(resourceUri)))
+
+        val output = blurBitmap(picture, applicationContext)
+
+        // Write bitmap to a temp file
+        val outputUri = writeBitmapToFile(applicationContext, output)
+
+        val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+        return outputData
     }
 }
