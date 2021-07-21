a557c140b492c72f0538b4d873dcfd985630e6a7
Caren Chang <carenc@google.com>
2021-07-21 10:45:01 -0700

    Delete SelectImageActivity and replace with static image

--- app/src/main/java/com/example/background/BlurApplication.kt
+++ app/src/main/java/com/example/background/BlurApplication.kt
@@ -17,14 +17,28 @@
 package com.example.background
 
 import android.app.Application
+import androidx.work.Configuration
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
-class BlurApplication() : Application() {
+class BlurApplication() : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
         }
     }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return if (BuildConfig.DEBUG) {
+            Configuration.Builder()
+                    .setMinimumLoggingLevel(android.util.Log.DEBUG)
+                    .build()
+        } else {
+            Configuration.Builder()
+                    .setMinimumLoggingLevel(android.util.Log.ERROR)
+                    .build()
+        }
+    }
+
 }
